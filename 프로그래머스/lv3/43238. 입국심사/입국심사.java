cdf8139149ce46822a10e5148bdef9defa93a430
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-05-12 21:51:12 +0900

    [level 3] Title: 입국심사, Time: 0.65 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        Arrays.sort(times);
+        
+        int max =times[times.length-1];
+        
+        long left = 0;
+        long right = (long) max * n;
+        
+        long min = Long.MAX_VALUE;
+        
+        while(left <= right){
+                
+            long mid = left+(right - left) /2;
+            if(check(mid, n, times)){
+                
+                right = mid-1;
+                min = Math.min(min,mid);
+                
+            }else{
+                left = mid+1;
+            }
+            
+        }
+        
+        return min;
+    }
+    
+    public boolean check(long k, long num, int[] times){
+        long answer = 0;
+        for(int i=0; i<times.length; i++){
+            int next = times[i];
+            answer+=  k / next;
+        }
+        
+        if(answer >= num) return true;
+        return false;
+    }
+}
