f4d86ca9644f76ae49496f7de116d159fde95f44
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-05-14 20:49:09 +0900

    [Silver III] Title: 숫자 야구, Time: 112 ms, Memory: 13232 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -3,69 +3,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-	
-	static String[] orders;
-	static int answer = 0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.valueOf(br.readLine());
-		
-		orders = new String[n];
-		
+class Main {
+	static int n;
+	static String[] cmds;
+	static int ans = 0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.valueOf(br.readLine());
+		cmds = new String[n];
 		for(int i=0; i<n; i++) {
-			orders[i] = br.readLine();
+			String n = br.readLine();
+			cmds[i] = n;
 		}
 		
-		dfs(0,"", new boolean[10]);
-		
-		System.out.println(answer);
-		
+		dfs(0,new boolean[10], new String[3]);
+		System.out.println(ans);
 	}
-	private static void dfs(int curIdx, String str, boolean[] ch) {
+	private static void dfs(int curIdx, boolean[] ch, String[] path) {
 		
 		if(curIdx == 3) {
 			
-			boolean result = true;
+			String next = path[0]+path[1]+path[2];
 			
-			for(String order : orders) {
+			for(int i=0; i<cmds.length; i++) {
 				
-				String[] or = order.split(" ");
-				
-				String guess = or[0];
-				int cnt = Integer.valueOf(or[1]);
-				int ball = Integer.valueOf(or[2]);
-				
-				int cntNum =0;
-				int ballNum = 0;
+				String[] guess = cmds[i].split(" ");
+				String num = guess[0];
+				int strike = Integer.valueOf(guess[1]);
+				int ball = Integer.valueOf(guess[2]);
 				
 				
-				for(int i=0; i<str.length(); i++) {
-					
-					char nextStr = str.charAt(i);
-
-					for(int j=0; j<guess.length(); j++) {
+				int str = 0;
+				int bal = 0;
+				for(int j=0; j<3; j++) {
+					char f = next.charAt(j);
+					for(int z =0; z<3; z++) {
 						
-						char nextGuess = guess.charAt(j);
+						char m = num.charAt(z);
 						
-						if(nextStr == nextGuess) {
-							if(i == j) cntNum++;
-							else ballNum ++;
+						if(f == m) {
+							if(j == z) str++;
+							else bal ++;
 						}
-						
 					}
+					
 				}
 				
-				if(cntNum == cnt && ball == ballNum) continue;
-				result = false;
+				if(str != strike || ball != bal) return;
 				
 				
 			}
-			
-			if(result) {
-				answer++;
-			}
-			
+			ans++;
 			
 		}else {
 			
@@ -73,7 +61,9 @@ public class Main {
 				
 				if(ch[i]) continue;
 				ch[i] = true;
-				dfs(curIdx+1,str+String.valueOf(i),ch);
+				path[curIdx] = i+"";
+				dfs(curIdx+1,ch,path);
+				path[curIdx] = null;
 				ch[i] = false;
 				
 			}
