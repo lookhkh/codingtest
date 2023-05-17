7d0ace9c7a5e0097da052145574045e729b10811
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-05-17 14:40:47 +0900

    [level 2] Title: 숫자의 표현, Time: 0.45 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,30 +1,34 @@
+/*
+    자연수 n에 대하여, n을 연속된 자연수의 합으로 표현하는 방법이 여러가지가 있을 수 있다.
+    이때, 경우의 수를 구하시오.
+    
+    20
+     2,3,4,5,6
+
+*/
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
         int left = 1;
-        int temp = 0;
-        for(int i=1; i<=n; i++){
+        int tempSum = 0;
+        
+        for(int right=1; right<=n; right++){
             
-            temp += i;
-            
-            if(temp >= n){
+            tempSum+=right;
+            if(tempSum >= n){
                 
-                while(temp >= n){
+                while(tempSum>=n){
                     
-                    if(temp == n) answer++;
-                    temp -= left;
+                    if(tempSum == n) answer++;
+                    tempSum -= left;
                     left++;
                     
                 }
                 
             }
             
-            
         }
-        
-        
-        
         
         return answer;
     }
