44a9d70a3a959e346ab454bfd11ac3426f916db9
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-05-24 16:48:48 +0900

    [level 2] Title: 택배상자, Time: 178.25 ms, Memory: 179 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -0,0 +1,60 @@
+/*
+    보조상자 => stack. 
+    컨테이너 위에는 일렬로 택배가 온다.
+    이때, 택배가 배달순서가 아닐 경우, 보조상자에 올려둔다.
+
+*/
+import java.util.*;
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> que = new LinkedList<>();
+        
+        for(int i=1; i<=order.length; i++) que.add(i);
+        
+        for(int i=0; i<order.length; i++){
+            
+            int nextOrder = order[i];
+                
+            //System.out.println(nextOrder+"  "+stack);
+            
+            if(!que.isEmpty() && que.peek() == nextOrder){
+                que.poll();
+                answer++;
+                continue;
+            }
+            
+           if(stack.isEmpty()) {
+               if(!que.isEmpty()) {
+                   stack.push(que.poll());
+                   i--;
+               }else{
+                    break;
+               }
+           }else{
+               
+               if(stack.peek() == nextOrder){
+                   stack.pop();
+                   answer++;
+               }else{
+                   if(!que.isEmpty()){
+                     stack.push(que.poll());
+                     i--;
+                   }else{
+                       break;
+                   }
+               }
+               
+               
+           }
+            
+            
+            
+            
+        }
+        
+        
+        return answer;
+    }
+}
