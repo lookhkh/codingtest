d4cf6cdaf322d59af712bfa1c18cd2c7c9d2a447
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-06-17 02:10:35 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 236 ms, Memory: 12176 KB -BaekjoonHub

--- 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
+++ 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
@@ -1,45 +1,67 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] array, check;
-	static int n, x, y,count;
-
-	public static void main(String[] args) throws Exception {
+	static int n;
+	static int x;
+	static int y;
+	static int[] arr;
+	static int cnt = 0;
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		x = Integer.parseInt(st.nextToken());
-		y = Integer.parseInt(st.nextToken());
-		count = 0;
+		StringTokenizer tk = new StringTokenizer(br.readLine()," ");
+		n = Integer.valueOf(tk.nextToken());
+		x = Integer.valueOf(tk.nextToken())-1;
+		y = Integer.valueOf(tk.nextToken())-1;
 		
-		array = new int[25];
-		check = new int[13];
-		array[x] = array[y] = y - x - 1;
-		check[y-x-1] = 1;
+		arr = new int[2*n];
+		int idx = 0;
+		for(int i=1; i<=n; i++) {
+			arr[idx++] = i;
+			arr[idx++] = i;
+		}
 		
-		BackTracking(1);
-		System.out.println(count);
+		int[] a = new int[2*n];
+		
+		boolean[] ch = new boolean[n+1];
+		
+		a[x] = a[y] = y-x-1;
+		ch[y-x-1] = true;
+		
+		dfs(2*n, 0, a, ch);
+		System.out.println(cnt);
 	}
-
-	private static void BackTracking(int index) {
-		if(index == 2*n){
-			count++; return;
-		}
-		if(array[index]==0){
-			for(int i=1; i<=n; i++){
-				if(check[i] == 1) continue;
-				if(index+i+1<=2*n && array[index+i+1] == 0){
-					array[index] = array[index+i+1] = i;
-					check[i] = 1;
-					BackTracking(index+1);
-					array[index] = array[index+i+1] = check[i] = 0;
+	private static void dfs(int tar, int cur, int[] r, boolean[] ch) {
+		
+		if(tar == cur) {
+			
+			cnt++;
+			return;
+			
+		}else {
+			
+			if(r[cur] ==0) {
+				
+				for(int i=1; i<=n; i++) {
+					if(ch[i]) continue;
+					if(cur+i+1 >= r.length || r[cur+i+1] !=0) continue;
+					
+					ch[i] = true;
+					r[cur]=r[cur+i+1] = i;
+					dfs(tar, cur+1, r, ch);
+					ch[i] = false;
+					r[cur]=r[cur+i+1] = 0;
 				}
+				
+			}else {
+				dfs(tar, cur+1, r, ch);
 			}
+			
+			
 		}
-	  else BackTracking(index+1);
+
 	}
 }
