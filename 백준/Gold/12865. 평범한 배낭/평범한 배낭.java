76b2f9268a4fa9a125e1597f66f1d68b78c28335
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-06-20 08:51:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 51408 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -24,21 +24,19 @@ public class Main {
 			vs[i] = v;
 		}
 		
-		long[][] dp = new long[n+1][m+1];
+		int[][] dp = new int[n+1][m+1];
 		
 		for(int i=1; i<dp.length; i++) {
-			int nw = ws[i];
-			int nv = vs[i];
-			for(int j=1; j<dp[i].length; j++) {
-				
-				if(nw > j) dp[i][j] = dp[i-1][j];
-				else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-nw]+nv);
+			for(int j=0; j<=m; j++) {
+				if(j-ws[i] < 0) {
+					dp[i][j] = dp[i-1][j];
+					continue;
 				}
+				
+				dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-ws[i]]+vs[i]);
 			}
 		}
 		
 		System.out.println(dp[n][m]);
-		
 	}
 }
