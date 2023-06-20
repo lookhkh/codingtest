3f8cd9c938dd600caacd50ae77069016be501edc
lookhkh <62491156+lookhkh@users.noreply.github.com>
2023-06-21 00:07:11 +0900

    [Silver I] Title: 포도주 시식, Time: 272 ms, Memory: 24628 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int[] arr = new int[n+1];
-		for(int i=1; i<=n; i++) {
+		int nums = in.nextInt();
+		int[] arr = new int[nums+1];
+		for(int i=1; i<=nums; i++) {
 			arr[i] = in.nextInt();
 		}
 		
-		int[][] dp = new int[n+1][2];
+		int[][] dp = new int[nums+1][2];
 		
 		dp[1][0] = dp[1][1] = arr[1];
 		
-		for(int i=2; i<=n; i++) {
+		for(int i=2; i<dp.length; i++) {
 			
-			dp[i][0] = Math.max(dp[i-1][1]+arr[i], dp[i-1][0]);
-			dp[i][1] = Math.max( Math.max(dp[i-2][0], dp[i-2][1]) + arr[i], dp[i-1][1]);
+			dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+arr[i]);
+			dp[i][1] = Math.max(dp[i-2][0], dp[i-2][1])+arr[i];
 			
 		}
 		
-		System.out.println(Math.max(dp[n][0], dp[n][1]));
-		
+		System.out.println(Math.max(dp[nums][0], dp[nums][1]));
+		int t =1;
 	}
 }
