98c5fe5026a255fdbbecc6154774291e4e2d8df9
lookhkh <62491156+lookhkh@users.noreply.github.com>
2024-02-18 15:45:44 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,63 +1,52 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	
-	static int ans = 0;
-	static List<List<Integer>> graph;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int k = Integer.valueOf(br.readLine());
-		int n = Integer.valueOf(br.readLine());
+    public static int cnt = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		graph = new ArrayList<>();
-		
-		for(int i=0; i<=k; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		for(int i=0; i<n; i++) {
-			
-			StringTokenizer tk = new StringTokenizer(br.readLine()," ");
-			int from = Integer.valueOf(tk.nextToken());
-			int to = Integer.valueOf(tk.nextToken());
-			
-			graph.get(from).add(to);
-			graph.get(to).add(from);
-			
-		}
-		
-		
-		boolean[] ch = new boolean[k+1];
-		ch[1] = true;
-		
-		dfs(1,ch);
-		
-		System.out.println(ans);
-		
-	}
-	private static void dfs(int start, boolean[] ch) {
-				
-		List<Integer> sub = graph.get(start);
-		
-		if(start!=1) {
-			ans++;
-		}
+        int nodes = Integer.parseInt(br.readLine());
+        int edges = Integer.parseInt(br.readLine());
 
-		for(int w : sub) {
-			
-			if(ch[w]) continue;
-			
-			ch[w] = true;
-			
-			dfs(w,ch);
-			
-			
-		}
-		
-	}
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int i=0; i<=nodes; i++) list.add(new ArrayList<>());
+
+        for(int i=0; i<edges; i++){
+
+            String[] split = br.readLine().split(" ");
+
+            int from = Integer.parseInt(split[0]);
+            int to = Integer.parseInt(split[1]);
+
+            list.get(from).add(to);
+            list.get(to).add(from);
+
+        }
+
+        sol(1, list);
+
+
+    }
+
+    private static void sol(int start, List<List<Integer>> list) {
+
+        boolean[] path = new boolean[list.size()];
+        dfs(start, path, list);
+        System.out.println(cnt);
+    }
+    private static void dfs(int start, boolean[] path, List<List<Integer>> list) {
+        if(path[start]) return;
+        path[start] = true;
+        cnt++;
+        List<Integer> integers = list.get(start);
+
+        for(int next : integers){
+            dfs(next, path, list);
+        }
+    }
 }
