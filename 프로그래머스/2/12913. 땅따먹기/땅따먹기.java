914e5ca0dfc76fddef2533280d2d78b9049978ba
lookhkh <62491156+lookhkh@users.noreply.github.com>
2024-03-10 00:41:04 +0900

    [level 2] Title: 땅따먹기, Time: 27.68 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -5,13 +5,16 @@ class Solution {
         for(int i=0; i<4; i++) r[0][i] = land[0][i];
         
         for(int i=1; i<land.length; i++){
-            r[i][0] = land[i][0]+Integer.max(Integer.max(r[i-1][1], r[i-1][2]), r[i-1][3]); 
-            r[i][1] = land[i][1]+Integer.max(Integer.max(r[i-1][0], r[i-1][2]), r[i-1][3]); 
-            r[i][2] = land[i][2]+Integer.max(Integer.max(r[i-1][0], r[i-1][1]), r[i-1][3]); 
-            r[i][3] = land[i][3]+Integer.max(Integer.max(r[i-1][0], r[i-1][1]), r[i-1][2]); 
+            r[i][0] = land[i][0]+max(max(r[i-1][1], r[i-1][2]), r[i-1][3]); 
+            r[i][1] = land[i][1]+max(max(r[i-1][0], r[i-1][2]), r[i-1][3]); 
+            r[i][2] = land[i][2]+max(max(r[i-1][0], r[i-1][1]), r[i-1][3]); 
+            r[i][3] = land[i][3]+max(max(r[i-1][0], r[i-1][1]), r[i-1][2]); 
         }
         
         
-        return Integer.max(Integer.max(Integer.max(r[land.length-1][0],r[land.length-1][1]),r[land.length-1][2]),r[land.length-1][3]);
+        return max(max(max(r[land.length-1][0],r[land.length-1][1]),r[land.length-1][2]),r[land.length-1][3]);
+    }
+    int max(int x, int y){
+        return Integer.max(x,y);
     }
 }
