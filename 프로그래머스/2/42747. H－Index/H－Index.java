3f6b62a67858fd26799a66fd65399f45ff15b05a
lookhkh <62491156+lookhkh@users.noreply.github.com>
2024-03-18 23:39:55 +0900

    [level 2] Title: H-Index, Time: 0.38 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i=0; i<citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length-i);
+            answer = Math.max(answer, smaller);
+        }
+        return answer;
+    }
+}
