d93fa9f32088b00426ccf709d167a082ab20480f
lookhkh <62491156+lookhkh@users.noreply.github.com>
2024-03-23 23:21:29 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.31 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        
+        int tIdx = 0;
+        int t = 0;
+        int weightSum = 0;
+        Queue<Truck> que = new LinkedList<>();
+        
+        while(true){
+            if(tIdx == truck_weights.length && que.isEmpty()) return t;
+            
+            t++;
+            
+            if(!que.isEmpty()){
+                Truck head = que.peek();
+                if(t - head.time >= bridge_length){
+                    Truck h = que.poll();
+                    weightSum -= h.weight;
+                }
+            }
+            
+            if(tIdx < truck_weights.length){
+                int curWeight = truck_weights[tIdx++];
+                weightSum += curWeight;
+
+                if(weightSum > weight) {
+                    weightSum -= curWeight;
+                    tIdx--;
+                    continue;
+                }
+
+                Truck nt = new Truck(t, curWeight);
+                que.add(nt);
+            }
+            
+        }
+    }
+}
+class Truck{
+    int weight;
+    int time;
+    Truck(int time, int weight){
+        this.time = time;
+        this.weight = weight;
+    }
+}
