ceca8acba9adcc17cff782044fcd1efba7133c4a
lookhkh <62491156+lookhkh@users.noreply.github.com>
2024-05-19 11:58:43 +0900

    [level 3] Title: 야근 지수, Time: 116.08 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Long> que = new PriorityQueue<>((t1,t2)->Long.compare(t2,t1));
+        
+        for(int work : works) que.add(Long.valueOf(work));
+        
+        while(n > 0 && que.peek()>0){
+            
+            long next = que.poll();
+            
+            next-=1;
+            que.add(next);
+            n--;
+            
+        }
+        
+        while(!que.isEmpty()) answer+=Math.pow(que.poll(),2);
+                
+        
+        return answer;
+    }
+}
